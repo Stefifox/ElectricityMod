8407933d3ae7937bca6a9de5339140b87f79e1c4
Stefano <30950154+Stefifox@users.noreply.github.com>
2021-02-25 17:53:08 +0100

    Added Electricity.cs file

---
+++ Electricity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+public class Electricity
+{
+	public static int PROGR = 0;
+
+	private float tW;
+	private int idRete;
+
+	public Electricity()
+	{
+		tW = 0f;
+		idRete = PROGR++;
+		Terraria.Chat.ChatMessage("[DEBUG]: Rete creata " + idRete.ToString() + " tW: " + tW.ToString());
+	}
+
+	public void setTW(float tW)
+    {
+		this.tW = tW;
+    }
+
+	public float getTW()
+    {
+		return this.tW;
+    }
+}
---
+++ Items/ScrewDriver.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ElectricityMod.Items
+{
+    class ScrewDriver : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Screw Driver");
+            Tooltip.SetDefault("Use this item to place cables");
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+
+    }
+}
